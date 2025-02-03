573dc87e817ac86b7b57f09f75be49c20b8b7929
yakirheshko <yakirhersko@gmail.com>
2025-02-03 21:34:50 +0200

    asd

---
+++ src/main/java/com/app/inv/InvApplication.java
@@ -0,0 +1,13 @@
+package com.app.inv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvApplication.class, args);
+    }
+
+}
